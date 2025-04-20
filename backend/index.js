a126a55545d9579066d9392e3f61ccd11a4498e7
Beyza <edabeyzaozeren@gmail.com>
2025-04-20 21:19:57 +0300

    up

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv')
+
+
+
+/* --------------------------------- dotenv --------------------------------- */
+dotenv.config()
